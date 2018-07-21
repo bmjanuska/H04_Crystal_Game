e9d198971237d21ec591c6c2ecda154a65c75729
Bernadette Januska <bmjanuska@gmail.com>
2018-07-21 19:53:56 -0400

    Solving for duplicate numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     //Varriable for the number you have to guess
-  var targetNumber = Math.floor(Math.random() * 101) +19;
+    var targetNumber = Math.floor(Math.random() * 101) +19;
 
   //Varriable that edits the DOM for the number we have to guess
   $(".guess_this_number").text(targetNumber);
@@ -8,65 +8,75 @@ $(document).ready(function(){
   //Our number that starts at zero
   var counter = 0;
 
-  //
+  //"Your Number" is chaning the DOM of to equal whatever is added
   $(".userTotal").text(counter);
 
-  //Counter that starts at zero 
+  //Wins that starts at zero 
   var wins = 0;
-
-  //
+  //Update DOM with wins
   $("#wins").text(wins);
 
-  //
-  var loss = 0;
+  //losseses that starts at zero 
+  var losses = 0;
+  //Update DOM with losses
+  $("#losses").text(losses);
 
-  //
-  $("#loss").text(loss);
-       
-       var targetNumber = Math.floor(Math.random() * 101) +19;
-       $('.guess_this_number').text(targetNumber);
-       
-       
+  //The computer generated number is determined by a math random
+  var targetNumber = Math.floor(Math.random() * 101) +19;
+  $('.guess_this_number').text(targetNumber);
+
+
       // Object array of all the crystals. Maps a random number to each crystal
       var numbers = {
-      "crystal_1": Math.floor(Math.random() * 11) +1,
-      "crystal_2": Math.floor(Math.random() * 11) +1,
-      "crystal_3": Math.floor(Math.random() * 11) +1,
-      "crystal_4": Math.floor(Math.random() * 11) +1,
+        "crystal_1": Math.floor(Math.random() * 11) +1,
+        "crystal_2": Math.floor(Math.random() * 11) +1,
+        "crystal_3": Math.floor(Math.random() * 11) +1,
+        "crystal_4": Math.floor(Math.random() * 11) +1,
       };
+      console.log(numbers)
       
       //For each 
-      // key is the cryston and the value is the math.floor 
-      //'#' edits our DOM
-     $.each(numbers, function(key, value){
+      // key is the crystal and the value is the math.floor 
+      $.each(numbers, function(key, value){
        $('.'+key).click(function(){
-          
 
-          counter += value;
-          
-         $('.userTotal').html(counter);
-         if(counter > targetNumber){
+        //Counter will increase from the value of each number
+        counter += value;
+
+        //Prevents same number appearing twice...maybe 
+        if ((numbers["crystal_1"]) === (numbers["crystal_2"]) || 
+            (numbers["crystal_1"]) === (numbers["crystal_3"]) ||
+            (numbers["crystal_1"]) === (numbers["crystal_4"]) ||
+            (numbers["crystal_2"]) === (numbers["crystal_3"]) ||
+            (numbers["crystal_2"]) === (numbers["crystal_4"]) ||
+            (numbers["crystal_3"]) === (numbers["crystal_4"])) {
+        };
+
+        //Counter class is updated with new number, 
+        //if go over the goal number lose, add loss, and reset
+        $('.userTotal').html(counter);
+        if(counter > targetNumber){
          $('#wlMessage').text("You Lose :(" );
-         loss ++;
-         $('#loss').text(loss);
-         counter = 0;
-         $(".userTotal").text(counter);
-                    $(".guess_this_number").text(Math.floor(Math.random() * 101) +19);
+           losses ++;
+           $('#losses').text(losses);
+           counter = 0;
+           $(".userTotal").text(counter);
+           $(".guess_this_number").text(Math.floor(Math.random() * 101) +19);
          }
 
+          //Counter class is updated with new number, 
+          //if user number equals goal numer its a win, add win, and reset
          if(counter === targetNumber){
-         $('#wlMessage').text("You Won!!!");
-         wins ++;
-         $('#wins').text(wins);
-                  counter = 0;
-         $(".userTotal").text(counter);
-                    $(".guess_this_number").text(Math.floor(Math.random() * 101) +19);
-
+           $('#wlMessage').text("You Won!!!");
+           wins ++;
+           $('#wins').text(wins);
+           counter = 0;
+           $(".userTotal").text(counter);
+           $(".guess_this_number").text(Math.floor(Math.random() * 101) +19);
          }
-
-
-
 
        });
-      });
-   });
+
+     });
+
+    });
