2bd369e0560a2f53d9371afa836120b67be6781d
Bernadette Januska <bmjanuska@gmail.com>
2018-07-21 13:57:28 -0400

    Js update

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -11,29 +11,39 @@
 
   $("#wins").text(wins);
 
+    var loss = 0;
+
+  $("#loss").text(loss);
+
+
   //Number options for what numbers the crystal can pick from 
   var numberOptions = [1,2,3,4,5,6,7,8,9,10,11,12]; 
 
+  //if the number a cyrstal picks is already selected it will pick another number
+ 
+
   // Next we create a for loop to create crystals for every numberOption.
   for (var i = 0; i < numberOptions.length; i++) {
-
-
     // Each imageCrystal will be given a data attribute called data-crystalValue.
     // This data attribute will be set equal to the array value.
-    cystal_1.attr("data-crystalvalue", numberOptions[i]);
+    $(".cystal_1").attr(numberOptions[i]);
 
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
+        $(".cystal_2").attr(numberOptions[i]);
+
+
+    $(".cystal_3").attr(numberOptions[i]);
+
+
+    $(".cystal_4").attr(numberOptions[i]);
+
+
   }
 
+
   // This time, our click event applies to every single crystal on the page. Not just one.
-  $(".crystal-image").on("click", function() {
+  $(".crystal_1").on("click", function() {
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
+   
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     // We then add the crystalValue to the user's "counter" which is a global variable.
