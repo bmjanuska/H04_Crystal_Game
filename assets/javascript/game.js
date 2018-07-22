fe134a6a5e8e000f32cf5c08b626b6fcbf0736fc
Bernadette Januska <bmjanuska@gmail.com>
2018-07-21 22:48:57 -0400

    Testing new duplicate method

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,90 +1,71 @@
     $(document).ready(function(){
         //Varriable for the number you have to guess
-        var targetNumber = Math.floor(Math.random() * 101) +19;
-    
+        var targetNumber = Math.floor(Math.random() * 101) + 19;
+
       //Varriable that edits the DOM for the number we have to guess
       $(".guess_this_number").text(targetNumber);
-    
+
       //Our number that starts at zero
       var counter = 0;
-    
+
       //"Your Number" is chaning the DOM of to equal whatever is added
       $(".userTotal").text(counter);
-    
+
       //Wins that starts at zero 
       var wins = 0;
       //Update DOM with wins
       $("#wins").text(wins);
-    
+
       //losseses that starts at zero 
       var losses = 0;
       //Update DOM with losses
       $("#losses").text(losses);
-    
+
       //The computer generated number is determined by a math random
-      var targetNumber = Math.floor(Math.random() * 101) +19;
+      var targetNumber = Math.floor(Math.random() * 101) + 19;
       $('.guess_this_number').text(targetNumber);
-    
-    
-          // Object array of all the crystals. Maps a random number to each crystal
-          var numbers = {
-            "crystal_1": Math.floor(Math.random() * 11) +1,
-            "crystal_2": Math.floor(Math.random() * 11) +1,
-            "crystal_3": Math.floor(Math.random() * 11) +1,
-            "crystal_4": Math.floor(Math.random() * 11) +1,
-          };
-          console.log(numbers)
+
+
+        // Object array of all the crystals. Maps a random number to each crystal
+          // Declare array for crystal
+          var crystal = ["crystal_1", "crystal_2", "crystal_3", "crystal_4" ];
           
-          //For each 
-          // key is the crystal and the value is the math.floor 
-          $.each(numbers, function(key, value){
-           $('.'+key).click(function(){
-    
-            //Counter will increase from the value of each number
-            counter += value;
-    
-            //Prevents same number appearing twice...maybe??? 
-            // if ((numbers["crystal_1"]) === (numbers["crystal_2"]) || 
-            //     (numbers["crystal_1"]) === (numbers["crystal_3"]) ||
-            //     (numbers["crystal_1"]) === (numbers["crystal_4"]) ||
-            //     (numbers["crystal_2"]) === (numbers["crystal_3"]) ||
-            //     (numbers["crystal_2"]) === (numbers["crystal_4"]) ||
-            //     (numbers["crystal_3"]) === (numbers["crystal_4"])) {
-            // }; nope no luck
+          // Declare var rndnum 
+          var rndnum = 0;
 
-            // or...? 
-            if ((numbers["crystal_1"]) === (numbers["crystal_2"])) {
-              (numbers["crystal_2"]) = Math.floor(Math.random() * 11) +1;
-              (numbers["crystal_1"]) != (numbers["crystal_2"]);
-            }; //no luck here either
+          // For loop each element in array
+          for ( var i = 0; i < crystal.length; i++){
+          console.log("this is my array: " + crystal);
+
+            // While rndnum is being tested for duplicates 
+            while (1) { 
+              // Get random number
+              // For each element 
+              rndnum = Math.floor(Math.random() * 12) + 1;
+
+              //Checking rndnum against next item in array
+              for ( var j = 0; j < crystal.length; j++){
+
+                // If rndnum is in array then test = false
+                if (rndnum === crystal[j]){
+                  // Restart while loop
+                  console.log("found a dup " + rndnum); break;
+                };
+              };
+
+              //Where you are adding random number to the array
+              crystal[i] = rndnum;
+              break;
+            };
+          };
+          
+          console.log("this is my array: " + crystal);
+
+          // test = true
+          // Place rndnum in array
+          // Exit while
 
 
-    
-            //Counter class is updated with new number, 
-            //if go over the goal number lose, add loss, and reset
-            $('.userTotal').html(counter);
-            if(counter > targetNumber){
-             $('#wlMessage').text("You Lose :(" );
-               losses ++;
-               $('#losses').text(losses);
-               counter = 0;
-               $(".userTotal").text(counter);
-               $(".guess_this_number").text(Math.floor(Math.random() * 101) +19);
-             }
-    
-              //Counter class is updated with new number, 
-              //if user number equals goal numer its a win, add win, and reset
-             if(counter === targetNumber){
-               $('#wlMessage').text("You Won!!!");
-               wins ++;
-               $('#wins').text(wins);
-               counter = 0;
-               $(".userTotal").text(counter);
-               $(".guess_this_number").text(Math.floor(Math.random() * 101) +19);
-             }
-    
-           });
-    
-         });
-    
+
+
         });
